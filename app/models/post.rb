a273e288c6b4ad653fbaf4c8e14fd5842123c369
Justin Lamb <justinlamb@me.com>
2022-10-05 12:03:17 -0700

    setup through associations

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates :link, presence: true, format: { with: URI.regexp }
+
+  belongs_to :user
+end
